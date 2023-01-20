fd855028ee48e950693aced95db58c96448ffe76
Ali <ali@example.com>
2023-01-20 22:51:55 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # learn-golangwith-chatgpt
 Learn golang with chatgpt
+
+# Install Go on Ubuntu Linux
+sudo apt update
+sudo apt install golang-go
+
+# Run go program
+go run main.go 
+go build main.go # to run the resulting binary executable file
+go install main.go # build and install package to the bin directory of GOPATH
+
---
+++ testvar.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 1
+	b := 1
+	c := a + b
+        fmt.Println("result of a + b is", c)
+	result := fmt.Sprintf("result with sprintf is %d", c)
+	fmt.Println(result)
+	fmt.Println("Test string concatenantion") 
+	
+	firstName := "Ali" 
+	lastName := "Ahmad" 
+        userName := firstName + " " + lastName
+	fmt.Println(userName)
+
+}
